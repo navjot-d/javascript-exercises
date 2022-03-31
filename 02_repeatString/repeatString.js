b0b27c3b99af4b701845867c0e243abb193eaf4e
nave <navedhesi@gmail.com>
2022-03-31 14:40:19 -0700

    Completeled exercises 01,02,03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(ourString, num) {
+    
+    let finalString = ''
 
+    if (num<0) {
+        return 'ERROR'
+    }
+    
+    for (i=0; i < num; i++) {
+        finalString += ourString;
+    }
+
+    return finalString
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(ourString) {
+    let ourStringArray = ourString.split("");
+    
+    ourStringArray = ourStringArray.reverse();
 
+    let reversedString = ''
+    
+    for (i=0; i < ourStringArray.length; i++) {
+        reversedString += ourStringArray[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
