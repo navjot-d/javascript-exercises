bcb0fdee8690683d538a749f0745503c0d8bea0b
nave <navedhesi@gmail.com>
2022-04-01 08:44:17 -0700

    Completed 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(ourArray, ...rmvs) {
+let numRemovals = rmvs.length;
+let newArray = [];
+
+for (i=0; i < rmvs.length; i++) {
+    
+    for (j=0; j < ourArray.length; j++) {
+        if (ourArray[j] === rmvs[i]) {
+            ourArray.splice(j, 1);
+        }
+    }
+}
+
+return ourArray
 
 };
 
