053da8e0408780547148b2dcd9de8171c890026b
nave <navedhesi@gmail.com>
2022-04-04 08:16:42 -0700

    completed 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+
+  let tempC = ((tempF - 32) * 5)/9;
+  tempC = Math.round(tempC * 10) / 10;
+  return tempC;
 
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+
+  let tempF = (tempC * 9) / 5 + 32;
+  tempF = Math.round(tempF * 10) / 10;
+  return tempF;
 
 };
 
