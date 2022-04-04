4d841eab5727ed3b5d8d95bfd180f8683861747c
nave <navedhesi@gmail.com>
2022-04-04 09:36:54 -0700

    completed 08.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+
+  let result = 0;
+
+	for (i=0; i < num.length; i++) {
+    result += num[i];
+  }
+
+  return result;
+
 };
 
-const multiply = function() {
+const multiply = function(num) {
+  let result = 1;
+
+  for (i=0; i < num.length; i++) {
+    result = result * num[i]
+  }
+
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+
+  if (num == 0) {
+    return 1;
+  }
+
+  for (i=num-1; i > 0; i--) {
+    result *= i
+  }
+return result;
 };
+
+factorial(5);
 
 // Do not edit below this line
 module.exports = {
