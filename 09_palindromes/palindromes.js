049b89ac8641c35b317f3abea7f967261998c045
nave <navedhesi@gmail.com>
2022-04-05 09:27:16 -0700

    completed 09.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (ourString) {
 
+    ourString = ourString.toLowerCase();
+    
+    ourString = ourString.replace(/[^a-z]/gi, '')
+
+    let ourStringArray = ourString.split("");
+
+    let reversedString = ourString.split("");
+    reversedString = reversedString.reverse();
+
+    //let reversedStringArray = ourStringArray.reverse();
+    
+    for (i=0; i < ourStringArray.length; i++) {
+        if (ourStringArray[i] != reversedString[i]) {
+            return false
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
