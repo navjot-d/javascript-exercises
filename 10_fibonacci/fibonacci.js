ba78491152a9f0e3e64b1cd12f51f6685d814bc6
nave <navedhesi@gmail.com>
2022-04-05 09:45:48 -0700

    completed 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    if (num <= 0) {
+        return 'OOPS'
+    }
+
+    let fibArray = [1,1];
+
+    for (i=2; i < num; i++) {
+            fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+
+    return fibArray[num-1];
 
 };
 
